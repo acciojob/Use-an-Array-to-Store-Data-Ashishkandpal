5d7774cf25000a34fc0557e528805c3c94e563d6
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-02-28 23:29:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 let yourArray = ["one", 2, 'three', true, false, undefined, null];
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ]
+];
 console.log(yourArray.length);
